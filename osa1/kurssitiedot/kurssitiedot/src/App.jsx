ecbc9bcaa6baabb24db07a68fc383265922144a0
Terhi Niemelä <terhi.niemela@gmail.com>
2024-02-16 21:21:47 +0200

    1.3. added

--- osa1/kurssitiedot/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/kurssitiedot/src/App.jsx
@@ -1,6 +1,7 @@
 // App renders course information
 // 1.1 code refactored to three components
 // 1.2 code includes now also component Part, which renders the information for course parts
+// 1.3 course information transformed to objects
 
 // header renders the name of the course
 const Header = (props) => {
@@ -43,18 +44,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part={[part1, part2, part3]} numberOfExercises={[exercises1, exercises2, exercises3]} />
-      <Total sumFirst={exercises1} sumSecond={exercises2} sumThird={exercises3} />
+      <Content part={[part1.name, part2.name, part3.name]} numberOfExercises={[part1.exercises, part2.exercises, part3.exercises]} />
+      <Total sumFirst={part1.exercises} sumSecond={part2.exercises} sumThird={part3.exercises} />
     </div>
   )
 }
