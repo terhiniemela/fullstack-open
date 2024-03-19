df20f1ad0a7e53f5d4b31c48b5b816a27fa4c816
Terhi Niemelä <terhi.niemela@gmail.com>
2024-03-19 20:53:06 +0200

    2.4 tehty

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -4,13 +4,26 @@
 // 1.3 course information transformed to objects
 // 2.1 rewriting components and adding component Course
 // 2.2. updated component Total 
+// 2.3. done already, in 2.2. Total was calculated with reduce
+// 2.4 updating app so that there can be as many courses as can be in the courses array
+
+
+// title for the page
+const Title = ({ title }) => {
+  console.log("hello from Title component")
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  )
+}
 
 // header renders the name of the course
 const Header = ({ title }) => {
   console.log("hello from Header component")
   return (
     <div>
-      <h1>{title}</h1>
+      <h2>{title}</h2>
     </div>
   )
 }
@@ -37,6 +50,7 @@ const Content = ({ parts }) => {
 }
 
 // renders the total sum of exercises from array parts
+// note that reduce needs one initial value first so it can calculate the sum, initial value is 0
 const Total = ({ parts }) => {
   console.log("hello from Total component")
   return (
@@ -59,36 +73,57 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'High-order functions',
-        exercises: 56,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
+
+  // we go through the courses array objects with map and call Course component for all courses
   return (
     <div>
-      <Course course={course} />
+      <Title title="Web development curriculum" />
+      {courses.map(course => <Course key={course.id} course={course} />)}
     </div>
   )
 }
