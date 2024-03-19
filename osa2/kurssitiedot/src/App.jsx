3dd6c62c7869045ab1c683ce140b869aff4794d9
Terhi Niemelä <terhi.niemela@gmail.com>
2024-03-19 20:02:12 +0200

    2.2 tehty

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -2,10 +2,12 @@
 // 1.1 code refactored to three components
 // 1.2 code includes now also component Part, which renders the information for course parts
 // 1.3 course information transformed to objects
-// 2.1 
+// 2.1 rewriting components and adding component Course
+// 2.2. updated component Total 
 
 // header renders the name of the course
 const Header = ({ title }) => {
+  console.log("hello from Header component")
   return (
     <div>
       <h1>{title}</h1>
@@ -15,6 +17,7 @@ const Header = ({ title }) => {
 
 // Part is used to render course information 
 const Part = ({ section, numberOfExercises }) => {
+  console.log("hello from Part component")
   return (
     <div>
       <p>{section} {numberOfExercises}</p>
@@ -25,6 +28,7 @@ const Part = ({ section, numberOfExercises }) => {
 // Content receives the information of course parts and numbers of exercises
 // in an array parts. Content of parts is mapped and forwarded to component Part
 const Content = ({ parts }) => {
+  console.log("hello from Content component")
   return (
     <div>
       {parts.map(part => <Part key={part.id} section={part.name} numberOfExercises={part.exercises} />)}
@@ -32,22 +36,24 @@ const Content = ({ parts }) => {
   )
 }
 
-// renders the total sum of exercises
+// renders the total sum of exercises from array parts
 const Total = ({ parts }) => {
+  console.log("hello from Total component")
   return (
     <div>
-      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises} </p>
+      <h3>Number of exercises {parts.reduce((sum, current) => sum + current.exercises, 0)}</h3>
     </div>
   )
 }
 
-// course component renders course information for a course
+// course component renders course information for a course and forwards information for other components
 const Course = ({ course }) => {
   console.log("hello from Course component")
   return (
     <div>
       <Header title={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
@@ -82,7 +88,6 @@ const App = () => {
 
   return (
     <div>
-      <Total parts={course.parts} />
       <Course course={course} />
     </div>
   )
