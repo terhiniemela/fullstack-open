42c7dc98ed131a5758a746a84f7f71b7340c185b
Terhi Niemelä <terhi.niemela@gmail.com>
2024-06-06 19:34:44 +0300

    puhelinluettelo 2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,45 +1,74 @@
-import { useState } from 'react'
-
-const ListPersons = () => {
-  persons.map((person) => <p key={person.name}> {person.name}</p>)
-}
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import phonebookService from './services/puhelinluettelo'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '041-22222' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
 
+  // state for adding persons in the notebook
+  const [persons, setPersons] = useState([])
+  // state for adding a new name to phonebook
+  const [newName, setNewName] = useState('')
+  // state for adding a new number to phonebook
+  const [newNumber, setNewNumber] = useState('')
+  // state for triggering showing all numbers in the phonebook or not
+  const [showAll, setShowAll] = useState(true)
+  // state for filtering names from the phonebook
+  const [newFilter, setNewFilter] = useState('')
+  // state for filtered phonebook 
+  const [filteredPhonebook, setFilteredPhonebook] = useState([])
+
+  // 2.11. changing the app to fetch data from json server using effect hooks
+  // notes: 
+  // - with parameter [] effect is run along only with the first render of the component
+  // - calling a state-updating function tringgers a re-rendering of the component
+  useEffect(() => {
+    console.log('effect')
+    phonebookService
+      // fetches data from the server
+      .getAll()
+      // event handler 
+      .then(response => {
+        console.log('promise fulfilled')
+        // setting the data from the server to state and 
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   // event handler for adding a person to phone book
   const addPerson = (event) => {
     event.preventDefault()
     console.log('button cklicket', event.target)
+
     const personObject = {
       name: newName,
       number: newNumber
     }
 
-    // person is added if the name isn't already listed
-    if (persons.find(person => (person.name !== newName))) {
-      console.log(newName)
-      console.log(persons.includes(newName))
-      setPersons(persons.concat(personObject))
-    }
-    else {
-      alert(`${newName} is already added to phonebook`)
-    }
+    // first: check if the name exists in the array of persons, if it exists: alert, if not: add name and number to list
+    persons.some(person => (person.name.toLowerCase() == newName.toLowerCase())) ? alert(`${newName} is already added to phonebook`) : setPersons(persons.concat(personObject))
+
     setNewName('')
     setNewNumber('')
   }
 
-  // event handler for handling form change 
+  // event handler for filter changes
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+    setShowAll(false)
+    const value = event.target.value.toLowerCase()
+    const filtered = persons.filter(person => person.name.toLowerCase().includes(value))
+    setFilteredPhonebook(filtered)
+  }
+
+  // event handler for handling form change for name
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  // event handler for handling form change for number
   const handleNumberChange = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
@@ -48,23 +77,54 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <h3>add a new</h3>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter value={newFilter} onChange={handleFilterChange} />
+
+      <Form onSubmit={addPerson} name={newName} handleNameChange={handleNameChange} number={newNumber} handleNumberChange={handleNumberChange} />
+
       <h2>Numbers</h2>
-      {persons.map((person) => <p key={person.name}> {person.name} {person.number}</p>)}
+      <Numbers showAllValue={showAll} persons={persons} filteredPhonebook={filteredPhonebook} />
     </div >
   )
+}
 
+// component Filter 
+const Filter = ({ filter, onChange }) => {
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={onChange} />
+    </div>
+  )
+}
+
+// component for the form where user can submit a new name and a number to the phonebook
+const Form = ({ onSubmit, name, handleNameChange, number, handleNumberChange }) => {
+  return (<form onSubmit={onSubmit}>
+    <h3>add a new</h3>
+    <div>
+      name: <input value={name} onChange={handleNameChange} />
+    </div>
+    <div>
+      number: <input value={number} onChange={handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+  )
+}
+
+
+// component for showing the phonebook and numbers
+const Numbers = ({ showAllValue, persons, filteredPhonebook }) => {
+  return (
+    <div>
+      {
+        showAllValue === true ?
+          persons.map(person => <p key={person.name}> {person.name} {person.number}</p>) :
+          filteredPhonebook.map((person) => <p key={person.name}> {person.name} {person.number}</p>)
+      }
+    </div>
+  )
 }
 
 export default App
